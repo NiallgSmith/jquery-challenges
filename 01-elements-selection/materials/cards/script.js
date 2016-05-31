bd81ea7881baa8b81ace35b97be25c6849410490
Niall Smith <niallsmi@gmail.com>
2016-05-31 16:53:57 +0100

    Mother of...  this stuffs getting complicated

--- 01-elements-selection/materials/cards/script.js
+++ 01-elements-selection/materials/cards/script.js
@@ -12,4 +12,22 @@ $(document).ready(function() {
 		$(".card").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
+	$("p").on("click",function() {
+		$("p").css("background-color", "red");
+	});
+	$("h2").on("mouseenter",function() {
+		$("h2").addClass("lightBlueBackground");
+	});
+	$("h2").on("mouseleave",function() {
+		$("h2").removeClass("lightBlueBackground");
+	});
+	$("h2").on("mouseover",function() {
+		$(this).css("font-size","2em");
+	});
+	$(".bottom_button").on("mouseenter",function() {
+		$("body").addClass("lightBlueBackground");
+	});
+	$(".bottom_button").on("mouseleave",function() {
+		$("body").removeClass("lightBlueBackground");
+	});
 });
